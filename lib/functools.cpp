b7957ddfea440ce3a19dae10160cd337a95dfed0
colindavis <colinmichaelsdavis@gmail.com>
2023-11-23 15:57:53 -0800

    add sorted

--- lib/functools.cpp
+++ lib/functools.cpp
@@ -26,6 +26,19 @@ void print (const std::tuple<T...>& _tup) {
 template <typename T>
 using vec = std::vector<T>;
 
+/*
+creates a sequence of length N, starting from the init and repeatedly applying f on the last value in the sequence
+*/
+auto recursive_seq(auto&& init, auto&& f, int N) {
+    std::vector<std::decay_t<decltype(init)>> result;
+    result.reserve(N);
+    result.push_back(std::forward<decltype(init)>(init));
+    for (auto i = 1; i < N; ++i) {
+        result.push_back(f(result.back()));
+    }
+    return result;
+}
+
 template <typename... Vecs>
 constexpr auto min_size(const Vecs&... vecs) {
     return std::min({vecs.size()...});
@@ -33,7 +46,14 @@ constexpr auto min_size(const Vecs&... vecs) {
 
 template <typename... Vecs>
 constexpr auto make_tuple_at_index(int index, const Vecs&... vecs) {
+    std::cout << "make tuple at index const lvalue ref\n";
     return std::make_tuple(vecs[index]...);
+}
+
+template <typename... Vecs>
+constexpr auto make_tuple_at_index(int index, Vecs&&... vecs) {
+    std::cout << "make tuple at index rvalue ref\n";
+    return std::make_tuple(std::move(vecs[index])...);
 }
 
 template <typename... Vecs>
@@ -47,20 +67,20 @@ auto zip(const Vecs&... vecs) {
     return result;
 }
 
-template <typename Vec>
-auto zip_with_indices(Vec&& vec) {
-    using Elem = typename std::decay_t<Vec>::value_type;
-    std::vector<std::tuple<int, Elem>> result;
-    const auto sz = vec.size();
+template <typename... Vecs>
+auto zip(Vecs&&... vecs) {
+    std::vector<std::tuple<typename std::decay_t<Vecs>::value_type...>> result;
+    const auto sz = min_size(vecs...);
     result.reserve(sz);
     for (auto i = 0; i < sz; ++i) {
-        if constexpr (std::is_nothrow_move_constructible_v<Elem>) {
-            result.emplace_back(i, std::move(vec[i]));
-        } else {
-            result.emplace_back(i, vec[i]);
-        }
+        result.push_back(make_tuple_at_index(i, std::forward<Vecs>(vecs)...));
     }
     return result;
+}
+
+template <typename Vec>
+auto zip_with_indices(Vec&& vec) {
+    return zip(recursive_seq(0, [](int x) { return x + 1; }, vec.size()), vec);
 }
 
 template <typename F, typename Vec, typename Init>
@@ -93,6 +113,10 @@ auto map(F&& f, const Vec& vec) {
     return result;
 }
 
+/*
+f must take in an argument and return a vector
+applies f to all elements of vec and merges the results into one vector
+*/
 auto flat_map(auto&& f, const auto& vec) {
     using Elem = typename std::decay_t<decltype(f(*std::begin(vec)))>::value_type;
     std::vector<Elem> result;
@@ -104,13 +128,23 @@ auto flat_map(auto&& f, const auto& vec) {
     return result;
 }
 
-// template <typename Vec>
-auto filter(auto&& f, auto&& vec) {
-    using Elem = typename std::decay_t<decltype(vec)>::value_type;
-    std::vector<Elem> result;
+template <typename T>
+auto filter(auto&& f, const std::vector<T>& vec) {
+    std::vector<T> result;
+    for (const auto& x : vec) {
+        if (f(x)) {
+            result.push_back(x);
+        }
+    }
+    return result;
+}
+
+template <typename T>
+auto filter(auto&& f, std::vector<T>&& vec) {
+    std::vector<T> result;
     for (auto&& x : vec) {
         if (f(x)) {
-            if constexpr (std::is_nothrow_move_constructible_v<Elem>) {
+            if constexpr (std::is_nothrow_move_constructible_v<T>) {
                 result.push_back(std::move(x));
             } else {
                 result.push_back(x);
@@ -125,8 +159,10 @@ auto deconstruct_impl(const std::vector<T>& vec, std::index_sequence<Is...>) {
     return std::make_tuple(vec[Is]..., vec[N - 1]);
 }
 
-// grab first N elements of a vector
-// useful in combination with structured binding
+/*
+places the first N elements of a vector in an N-tuple
+useful in combination with structured binding
+*/
 template <std::size_t N, typename T>
 auto deconstruct(const std::vector<T>& vec) {
     if (vec.size() < N) {
@@ -138,36 +174,33 @@ auto deconstruct(const std::vector<T>& vec) {
 template <typename T>
 concept BooleanConvertible = std::convertible_to<T, bool>;
 
+/*
+returns true if and only if all of the arguments are true
+*/
 template <BooleanConvertible... Args>
 bool all_of(Args... args) {
     return (args && ...);
 }
 
+/*
+returns true if and only if at least one of the arguments is true
+*/
 template <BooleanConvertible... Args>
 bool one_of(Args... args) {
     return (args || ...);
 }
 
+/*
+returns true if and only if all of the arguments are false
+*/
 template <BooleanConvertible... Args>
 bool none_of(Args... args) {
     return (!args && ...);
 }
 
-template <typename T>
-concept PartialEq = requires(T a, T b) {
-    {a < b} -> std::convertible_to<bool>;
-    {a == b} -> std::convertible_to<bool>;
-};
-
-// half-open interval
-template <PartialEq T>
-auto range_filter(T&& a, T&& b) {
-    return
-        [a, b](T c) { 
-            return (a == c || a < c) && c < b;
-        };
-}
-
+/*
+returns a function that applies the input functions from left to right on its inputs
+*/
 auto pipeline(auto&& first, auto&&... rest) {
     if constexpr (sizeof...(rest) == 0) {
         return first;
@@ -178,14 +211,27 @@ auto pipeline(auto&& first, auto&&... rest) {
     }
 }
 
-auto recursive_seq(auto&& init, auto&& f, int N) {
-    std::vector<std::decay_t<decltype(init)>> result;
-    result.reserve(N);
-    result.push_back(std::forward<decltype(init)>(init));
-    for (auto i = 1; i < N; ++i) {
-        result.push_back(f(result.back()));
-    }
-    return result;
+auto sorted(const auto& xs) {
+    auto new_xs = xs;
+    sort(new_xs.begin(), new_xs.end());
+    return new_xs;
+}
+
+template <typename T>
+concept PartialEq = requires(T a, T b) {
+    {a < b} -> std::convertible_to<bool>;
+    {a == b} -> std::convertible_to<bool>;
+};
+
+/*
+half-open interval
+*/
+template <PartialEq T>
+auto range_filter(T&& a, T&& b) {
+    return
+        [a, b](T c) { 
+            return (a == c || a < c) && c < b;
+        };
 }
 
 // };  // namespace functools
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,20 @@
 #include <vector>
 #include <stdexcept>
 
-void printvec(auto&& vec) {
-    for (const auto x : vec) {
-        std::cout << x << "" << '\n';
+
+template <typename T>
+void f(T) {
+    const auto z = std::convertible_to<T, bool>;
+    if constexpr(std::convertible_to<T, bool>) {
+        std::cout << "T is convertible to a bool\n";
+    } else {
+        std::cout << "T is not convertible to a bool\n";
     }
 }
 
-int h(int x) { return x + 100; }
-int f(int x) { return 2 * x; }
-
 int main() {
-    const auto g = pipeline(f, h);
-    // std::cout << pipeline(f, h)(1) << ' ' << pipeline(h, f)(1) << '\n';
-    printvec(recursive_seq(1, f, 4));
-    // std::cout << g(3) << '\n';
-    // std::cout << none_of(false, 0) << '\n';
-    // print(f(1, 2, 3));
+    vec<int> xs{8, 4, 9, 1};
+    for (const auto& z : zip_with_indices(sorted(xs))) {
+        print(z);
+    }
 }
